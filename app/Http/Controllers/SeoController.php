e006c088a2ebbe8cb4d2e48bc27eb7522b308a6d
ardi7923 <ardiirawan777@gmail.com>
2021-04-21 22:50:39 +0800

    selection statistik done

--- app/Http/Controllers/SeoController.php
+++ app/Http/Controllers/SeoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Keyword;
 use DataTables;
+use Carbon\Carbon;
 
 class SeoController extends Controller
 {
@@ -17,13 +18,25 @@ class SeoController extends Controller
         return view('pages.seo.table');
     }
 
-    public function statistik()
+    public function statistik(Request $request)
     {
-        $keywords = Keyword::limit(10)->get()->unique('keyword');
-        foreach($keywords as $k){
-            $k->frequency = Keyword::where('keyword',$k->keyword)->count();
+        $month = $request->month;
+        if($request->month){
+            $keywords = Keyword::whereMonth('created_at',$request->month)->limit(10)->get()->unique('keyword');
+            foreach($keywords as $k){
+                $k->frequency = Keyword::where('keyword',$k->keyword)->whereMonth('created_at',$request->month)->count();
+            }
+            $collection = collect($keywords)->sortByDesc('frequency');
+        }else{
+            $keywords = Keyword::limit(10)->get()->unique('keyword');
+            foreach($keywords as $k){
+                $k->frequency = Keyword::where('keyword',$k->keyword)->count();
+            }
+            $collection = collect($keywords)->sortByDesc('frequency');
         }
-        return view('pages.seo.statistik',compact('keywords'));
+        
+
+        return view('pages.seo.statistik',compact('collection','month'));
     }
 
     private function datatable()
--- resources/views/pages/seo/statistik.blade.php
+++ resources/views/pages/seo/statistik.blade.php
@@ -8,46 +8,66 @@
 @section('content')
 <div class="container-fluid">
 
-  <div class="col-lg-12 mb-4">
-    <div class="card bg-danger text-white shadow">
-      <div class="card-body">
-        Data yg ditampilkan hanya 10 Keyword terbanyak 
-      </div>
-    </div>
-  </div>
-
-  <div class="row">
-
-    <!-- Area Chart -->
-    <div class="col-xl-12 col-lg-12">
-      <div class="card shadow mb-4">
-        <!-- Card Header - Dropdown -->
-        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-          <h6 class="m-0 font-weight-bold text-primary">Statistik Keyword</h6>
-          <div class="dropdown no-arrow">
-            <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-              <div class="dropdown-header">Dropdown Header:</div>
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
+    <div class="col-lg-12 mb-4">
+        <div class="card bg-danger text-white shadow">
+            <div class="card-body">
+                Data yg ditampilkan hanya 10 Keyword terbanyak
             </div>
-          </div>
         </div>
-        <!-- Card Body -->
-        <div class="card-body">
-          <div>
-            <canvas id="myChart" width="400" height="200"></canvas>
-          </div>
-        </div>
-      </div>
     </div>
 
+    <div class="row">
 
-  </div>
+        <!-- Area Chart -->
+        <div class="col-xl-12 col-lg-12">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Statistik Keyword</h6>
+                    <div class="dropdown no-arrow">
+                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                            <div class="dropdown-header">Dropdown Header:</div>
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    <div>
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Pilih Bulan</label>
+                            <div class="col-sm-9">
+                                <select id="month" class="form-control">
+                                    <option disabled selected>--PILIH-- </option>
+                                    <option value="1"> Januari </option>
+                                    <option value="2"> Februari </option>
+                                    <option value="3"> Maret </option>
+                                    <option value="4"> April </option>
+                                    <option value="5"> Mei </option>
+                                    <option value="6"> Juni </option>
+                                    <option value="7"> Juli </option>
+                                    <option value="8"> Agustus </option>
+                                    <option value="9"> September </option>
+                                    <option value="10"> Oktober </option>
+                                    <option value="11"> November </option>
+                                    <option value="12"> Desember </option>
+                                </select>
+                            </div>
+                        </div>
+                        <canvas id="myChart" width="400" height="200"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
 
 
 
@@ -60,54 +80,64 @@
 
 @section('js')
 <script>
-  var statistiks = [];
-  var label = [];
-  @foreach($keywords as $u)
-  statistiks.push('{{$u->frequency}}');
-  label.push('{{{ $u->keyword }}}')
-  @endforeach
+    $(document).ready(function(){
+        $('#month').val('{{ $month }}');
+    });
 
-  console.log(statistiks);
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'horizontalBar',
-    data: {
-      labels: label,
-      datasets: [{
-        label: 'pencarian',
-        data: statistiks,
-        backgroundColor: [
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-        ],
-        borderColor: [
-          'rgba(75, 192, 192, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(75, 192, 192, 1)',
+    var statistiks = [];
+    var label = [];
+    @foreach($collection as $u)
+    statistiks.push('{{$u->frequency}}');
+    label.push('{{{ $u->keyword }}}')
+    @endforeach
 
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      indexAxis: 'y',
+    console.log(statistiks);
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: {
+            labels: label,
+            datasets: [{
+                label: 'pencarian',
+                data: statistiks,
+                backgroundColor: [
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(75, 192, 192, 1)',
 
-    }
-  });
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            indexAxis: 'y',
+
+        }
+    });
+
+    $('#month').change(function(){
+        data = $(this).val();
+        
+        window.location.replace("{{ url('seo-statistik') }}"+"?month="+data);
+    });
 </script>
 @endsection
